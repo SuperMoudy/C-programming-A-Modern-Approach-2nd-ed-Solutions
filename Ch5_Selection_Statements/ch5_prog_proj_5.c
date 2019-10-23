12f57719e5136a125c80094162a3a51e5196dcbf
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-10-23 12:26:57 +0200

    add ch5 prog proj 5

---
+++ Ch5_Selection_Statements/ch5_prog_proj_5.c
@@ -0,0 +1,48 @@
+/*
+ * ch5_prog_proj_5.c
+ *
+ *  Created on: Oct 23, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 5: tax income
+#include <stdio.h>
+
+int main(void)
+{
+	float  income, tax = 0.0f;
+
+	printf("Enter the taxable income: ");
+	fflush(stdout);
+	scanf("%f", &income);
+
+	if(income > 0 && income <= 750)
+	{
+		tax = 0.01f * income;
+	}
+	else if(income > 750 && income <= 2250)
+	{
+		tax = 7.5f + 0.02f * income;
+	}
+	else if(income > 2250 && income <= 3750)
+	{
+		tax = 37.50f + 0.03f * income;
+	}
+	else if(income > 3750 && income <= 5250)
+	{
+		tax = 82.50f + 0.04f * income;
+	}
+	else if(income > 5250 && income <= 7000)
+	{
+		tax = 142.50f + 0.05f * income;
+	}
+	else if(income > 7000)
+	{
+		tax = 230.00f + 0.06f * income;
+	}
+
+	printf("Tax due = %f", tax);
+
+	return 0;
+
+}
