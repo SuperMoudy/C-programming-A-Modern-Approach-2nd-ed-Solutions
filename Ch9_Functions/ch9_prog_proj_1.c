89e97491fafa674a3d6935c271a5e458b830d1db
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-10-23 09:50:45 +0200

    add ch9 prog proj 1

---
+++ Ch9_Functions/ch9_prog_proj_1.c
@@ -0,0 +1,65 @@
+/*
+ * ch9_prog_proj_1.c
+ *
+ *  Created on: Oct 23, 2019
+ *      Author: SuperMoudy
+ */
+
+
+// Programming Project 1: Selection sort
+#include <stdio.h>
+
+void selection_sort(int a[], int n);
+
+int main(void)
+{
+	int n;
+
+	printf("Enter the number of integers to be entered: ");
+	fflush(stdout);
+	scanf("%d", &n);
+
+	int a[n];
+
+	printf("Enter the list of numbers: ");
+	fflush(stdout);
+
+	for(int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	selection_sort(a, n);
+
+	printf("Array after sorting: ");
+
+	for(int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+
+	return 0;
+}
+
+
+void selection_sort(int a[], int n)
+{
+	if(n == 1)
+		return;
+
+	int max_element_value = a[0], max_element_index = 0;
+	for(int i = 1; i < n; i++)
+	{
+		if(a[i] > max_element_value)
+		{
+			max_element_value = a[i];
+			max_element_index = i;
+		}
+	}
+
+	// Moving to last position
+	a[max_element_index] = a[n - 1];
+	a[n - 1] = max_element_value;
+
+	selection_sort(a, n - 1);
+}
