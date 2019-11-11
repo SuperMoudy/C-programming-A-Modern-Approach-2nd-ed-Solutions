aef4bc6f046de39cb885db25e4a92792b7b05722
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-11-11 23:32:59 +0200

    add ch10 prog proj 6

---
+++ Ch10_Program_Organization/ch10_prog_proj_06.c
@@ -0,0 +1,142 @@
+/*
+ * ch10_prog_proj_06.c
+ *
+ *  Created on: Nov 11, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 6: RPN expressions
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define STACK_SIZE 100
+
+// External Variables
+int contents[STACK_SIZE];
+int top = 0;
+
+// Stack prototypes
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(int i);
+int pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
+
+int main(void)
+{
+	char ch;
+	int op1, op2, result;
+
+	printf("Enter an RPN expression: ");
+	fflush(stdout); fflush(stdin);
+
+	while(1)
+	{
+		ch = getchar();
+
+		if(ch == ' ')
+		{
+			continue;
+		}
+		else if(ch >= '0' && ch <= '9')
+		{
+			push(ch - '0');
+		}
+		else if(ch == '+')
+		{
+			op2 = pop();
+			op1 = pop();
+			result = op1 + op2;
+			push(result);
+		}
+		else if(ch == '-')
+		{
+			op2 = pop();
+			op1 = pop();
+			result = op1 - op2;
+			push(result);
+		}
+		else if(ch == '*')
+		{
+			op2 = pop();
+			op1 = pop();
+			result = op1 * op2;
+			push(result);
+		}
+		else if(ch == '/')
+		{
+			op2 = pop();
+			op1 = pop();
+			result = op1 / op2;
+			push(result);
+		}
+		else if(ch == '=')
+		{
+			result = pop();
+		}
+		else if(ch == '\n')
+		{
+			printf("Value of expression: %d\n", result);
+			printf("Enter an RPN expression: ");
+			fflush(stdout); fflush(stdin);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
+
+void make_empty(void)
+{
+	top = 0;
+}
+
+bool is_empty(void)
+{
+	return top == 0;
+}
+
+bool is_full(void)
+{
+	return top == STACK_SIZE;
+}
+
+void push(int i)
+{
+	if(is_full())
+	{
+		stack_overflow();
+	}
+	else
+	{
+		contents[top++] = i;
+	}
+}
+
+int pop(void)
+{
+	if(is_empty())
+	{
+		stack_underflow();
+	}
+
+	return contents[--top];
+}
+
+void stack_overflow(void)
+{
+	printf("Expression is too complex");
+	exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void)
+{
+	printf("Not enough operands in expression");
+	exit(EXIT_FAILURE);
+}
