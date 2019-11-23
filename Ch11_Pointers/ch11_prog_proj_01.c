d66e0e4972328699c8d3b036c85d3b01c61aea52
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-11-23 19:14:32 +0200

    add ch11 prog proj 1

---
+++ Ch11_Pointers/ch11_prog_proj_01.c
@@ -0,0 +1,51 @@
+/*
+ * ch11_prog_proj_01.c
+ *
+ *  Created on: Nov 23, 2019
+ *      Author: SuperMoudy
+ */
+
+// Project 1: Smallest number of bills
+
+#include <stdio.h>
+
+void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones);
+
+int main(void)
+{
+	int USD_amount, _20_USD = 0, _10_USD = 0, _5_USD = 0, _1_USD = 0;
+
+	printf("Enter a dollar amount: ");
+	fflush(stdout); fflush(stdin);
+	scanf("%d", &USD_amount);
+
+	pay_amount(USD_amount, &_20_USD, &_10_USD, &_5_USD, &_1_USD);
+
+	printf("$20 bills: %d\n$10 bills: %d\n $5 bills: %d\n $1 bills: %d",
+			_20_USD, _10_USD, _5_USD, _1_USD);
+
+	return 0;
+}
+
+void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones)
+{
+	if(dollars > 20)
+	{
+		*twenties = dollars / 20;
+		dollars %= 20;
+	}
+
+	if(dollars > 10)
+	{
+		*tens = dollars / 10;
+		dollars %= 10;
+	}
+
+	if(dollars > 5)
+	{
+		*fives = dollars / 5;
+		dollars %= 5;
+	}
+
+	*ones = dollars;
+}
