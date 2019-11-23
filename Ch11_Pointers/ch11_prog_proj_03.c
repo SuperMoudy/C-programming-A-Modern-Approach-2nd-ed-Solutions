d284605e0d635af94dd613606b2386ab9f6fa14d
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-11-23 20:51:58 +0200

    add ch11 prog proj 3

---
+++ Ch11_Pointers/ch11_prog_proj_03.c
@@ -0,0 +1,45 @@
+/*
+ * ch11_prog_proj_03.c
+ *
+ *  Created on: Nov 23, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 3: Fraction's lowest terms (GCD)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void reduce(int numerator, int denominator, int *reduced_numerator, int *reduced_denominator);
+int gcd(int a, int b);
+
+int main(void)
+{
+	int num1, den1, num2, den2;
+
+	printf("Enter a fraction: ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%d/%d", &num1, &den1);
+
+	reduce(num1, den1, &num2, &den2);
+
+	printf("In lowest terms: %d/%d", num2, den2);
+
+	return 0;
+}
+
+void reduce(int numerator, int denominator, int *reduced_numerator, int *reduced_denominator)
+{
+	int hcf = gcd(numerator, denominator);
+	(*reduced_numerator) = numerator / hcf;
+	(*reduced_denominator) = denominator / hcf;
+}
+
+int gcd(int a, int b)
+{
+	if(b == 0)
+		return a;
+
+	return gcd(b, a % b);
+}
