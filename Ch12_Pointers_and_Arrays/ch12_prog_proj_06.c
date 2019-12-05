5a4d7f9049aec7e85ce29fb82c55ca97164a0520
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-05 05:47:44 +0200

    add ch12 prog proj 6

---
+++ Ch12_Pointers_and_Arrays/ch12_prog_proj_06.c
@@ -0,0 +1,75 @@
+/*
+ * ch12_prog_proj_06.c
+ *
+ *  Created on: Dec 5, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 6: Quicksort algorithm
+
+#include <stdio.h>
+#define N 10
+
+void quicksort(int a[], int *low, int *high);
+int *split(int a[], int *low, int *high);
+
+int main(void)
+{
+	int a[N], i;
+
+	printf("Enter %d numbers to be stored: ", N);
+	fflush(stdout);
+	fflush(stdin);
+
+	for(i = 0; i < N; i++)
+		scanf("%d", &a[i]);
+
+	quicksort(a, &a[0], &a[N - 1]);
+
+	printf("In sorted order: ");
+	for(i = 0; i < N; i++)
+		printf("%d ", a[i]);
+
+	printf("\n");
+
+	return 0;
+}
+
+void quicksort(int a[], int *low, int *high)
+{
+	int *middle;
+
+	if(low >= high)
+		return;
+
+	middle = split(a, low, high);
+	quicksort(a, low, middle - 1);
+	quicksort(a, middle + 1, high);
+}
+
+int *split(int a[], int* low, int* high)
+{
+	int part_element = *low;
+
+	for(;;)
+	{
+		while(low < high && part_element <= *high)
+			high--;
+
+		if(low >= high)
+			break;
+
+		*low++ = *high;
+
+		while(low < high &&  *low <= part_element)
+			low++;
+
+		if(low >= high)
+			break;
+
+		*high-- = *low;
+	}
+
+	*high = part_element;
+	return high;
+}
