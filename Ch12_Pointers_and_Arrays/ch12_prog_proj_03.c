250fd7cabe75f4199a39abd403ec34991bf0ab95
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-05 03:17:03 +0200

    add ch12 prog proj 3

---
+++ Ch12_Pointers_and_Arrays/ch12_prog_proj_03.c
@@ -0,0 +1,38 @@
+/*
+ * ch12_prog_proj_03.c
+ *
+ *  Created on: Dec 5, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 3: Message Reversal (Same as 1.b)
+
+#include <stdio.h>
+#define LEN 100
+
+int main(void)
+{
+	char input, message[LEN] = {0}, *msg_ptr = message;
+
+	printf("Enter a message: ");
+	fflush(stdout);
+	fflush(stdin);
+
+	// Reading the message
+	input = getchar();
+
+	while(input != '\n' && msg_ptr < message + LEN)
+	{
+		*msg_ptr++ = input;
+		input = getchar();
+	}
+	msg_ptr--;
+
+	printf("Reversal is: ");
+	while(msg_ptr >= message)
+	{
+		printf("%c", *msg_ptr--);
+	}
+
+	return 0;
+}
