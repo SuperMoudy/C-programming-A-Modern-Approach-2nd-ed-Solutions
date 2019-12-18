0c2ef1cc6c1fc70db38ed0c135bcf5b37d184b3a
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-18 12:23:49 +0200

    add ch13 prog proj 5

---
+++ Ch13_Strings/ch13_prog_proj_05/sum.c
@@ -0,0 +1,24 @@
+/*
+ * sum.c
+ *
+ *  Created on: Dec 18, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 5: Adding up command-line arguments
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	while(i < argc)
+		sum += atoi(argv[i++]);
+
+	printf("Total: %d\n", sum);
+
+	return 0;
+}
