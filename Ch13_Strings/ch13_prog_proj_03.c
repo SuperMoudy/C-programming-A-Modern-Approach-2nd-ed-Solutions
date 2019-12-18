a17e5a05784e3b3d321c186db17750253663e171
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-18 06:40:14 +0200

    add ch13 prog proj 3

---
+++ Ch13_Strings/ch13_prog_proj_03.c
@@ -0,0 +1,55 @@
+/*
+ * ch13_prog_proj_03.c
+ *
+ *  Created on: Dec 18, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 3: Dealing random hand of cards(8.2 deal.c)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define NUM_SUITS 4
+#define NUM_RANKS 13
+
+int main(void)
+{
+	bool in_hand[NUM_SUITS][NUM_RANKS] = {{false}};
+	int num_cards, rank, suit;
+
+	const char *rank_code[] =
+	{
+			"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+			"Ten", "Jack", "Queen", "King", "Ace"
+	};
+
+	const char *suit_code[] = {"clubs", "diamonds", "hearts", "spaces"};
+
+	srand((unsigned) time(NULL)); // Setting a seed
+
+	printf("Enter number of cards in hand: ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%d", &num_cards);
+
+	printf("You hand:\n");
+
+	while(num_cards > 0)
+	{
+		suit = rand() % NUM_SUITS;
+		rank = rand() % NUM_RANKS;
+
+		if(!in_hand[suit][rank])
+		{
+			in_hand[suit][rank] = true;
+			num_cards--;
+			printf("%s of %s\n", rank_code[rank], suit_code[suit]);
+		}
+	}
+	printf("\n");
+
+	return 0;
+}
