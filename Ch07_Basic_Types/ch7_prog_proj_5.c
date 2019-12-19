18ea27f1d1975b228a5efd1a45f1413224891dbf
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-19 09:28:20 +0200

    add ch7 prog proj 5

---
+++ Ch07_Basic_Types/ch7_prog_proj_5.c
@@ -0,0 +1,79 @@
+/*
+ * ch7_prog_proj_5.c
+ *
+ *  Created on: Dec 19, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 5: Scrabble
+
+// Faster implementation (Tradeoff between memory and speed)
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int face_value[26] =
+	{
+			1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+	};
+
+	char input_ch;
+	int score = 0;
+
+	printf("Enter a word: ");
+	fflush(stdout);
+	fflush(stdin);
+
+	while((input_ch = getchar()) != '\n')
+	{
+		if(input_ch >= 'A' && input_ch <= 'z')
+			score += face_value[toupper(input_ch) - 'A'];
+	}
+
+	printf("Scrabble value: %d\n", score);
+
+	return 0;
+}
+
+// Slower Implementation (Treadeoff between memory and speed)
+/*
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char input_ch;
+	int score = 0;
+
+	printf("Enter a word: ");
+	fflush(stdout);
+	fflush(stdin);
+
+	while((input_ch = getchar()) != '\n')
+	{
+		switch(toupper(input_ch))
+		{
+		case 'A': case 'E': case 'I': case 'L': case 'N': case 'O': case 'R':
+		case 'S': case 'T': case 'U': score++; break;
+
+		case 'D': case 'G': score += 2; break;
+
+		case 'B': case 'C': case 'M': case 'P': score += 3; break;
+
+		case 'F': case 'H': case 'V': case 'W': case 'Y': score += 4; break;
+
+		case 'K': score += 5; break;
+
+		case 'J': case 'X': score += 8; break;
+
+		case 'Q': case 'Z': score += 10; break;
+
+		}
+	}
+
+	printf("Scrabble value: %d\n", score);
+
+	return 0;
+}
+*/
