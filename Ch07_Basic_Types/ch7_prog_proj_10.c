8586b7b60807e70c3c81cf5f90ccec8ad3501316
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-19 10:18:55 +0200

    add ch7 prog proj 10

---
+++ Ch07_Basic_Types/ch7_prog_proj_10.c
@@ -0,0 +1,34 @@
+/*
+ * ch7_prog_proj_10.c
+ *
+ *  Created on: Dec 19, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 10: Number of Vowels
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char input_ch;
+	int num_vowels = 0;
+
+	printf("Enter a sentence: ");
+	fflush(stdout);
+	fflush(stdin);
+
+	while((input_ch = getchar()) != '\n')
+	{
+		switch(toupper(input_ch))
+		{
+		case 'A': case 'E': case 'I': case 'O': case 'U':
+			num_vowels++;
+		}
+	}
+
+	printf("Your sentence contains %d vowels\n", num_vowels);
+
+	return 0;
+}
