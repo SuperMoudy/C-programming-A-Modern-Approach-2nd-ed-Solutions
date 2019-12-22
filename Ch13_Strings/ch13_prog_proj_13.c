cabb1bf9f1b83451f725ed4fd21110763c88462f
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-22 10:49:14 +0200

    add ch13 prog proj 13

---
+++ Ch13_Strings/ch13_prog_proj_13.c
@@ -0,0 +1,55 @@
+/*
+ * ch13_prog_proj_13.c
+ *
+ *  Created on: Dec 22, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 13: Caesar Cipher
+
+#include <stdio.h>
+
+void encrypt(char *message, int shift);
+
+int main(void)
+{
+	char input_ch, plain[80];
+	int msg_len = 0, key;
+
+	printf("Enter message to be encrypted: ");
+	fflush(stdout);
+	fflush(stdin);
+
+	while((input_ch = getchar()) != '\n')
+	{
+		plain[msg_len++] = input_ch;
+	}
+	plain[msg_len] = '\0'; // Turn the plain array into a string
+
+	printf("Enter shift amount (1-25): ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%d", &key);
+
+	encrypt(plain, key);
+	printf("Encrypted message: %s\n", plain);
+
+	return 0;
+}
+
+
+void encrypt(char *message, int shift)
+{
+	while(*message)
+	{
+		if(*message >= 'A' && *message <= 'Z')
+		{
+			*message = ((*message - 'A') + shift) % 26 + 'A';
+		}
+		else if(*message > 'Z' && *message <= 'z')
+		{
+			*message = ((*message - 'a') + shift) % 26 + 'a';
+		}
+		message++;
+	}
+}
