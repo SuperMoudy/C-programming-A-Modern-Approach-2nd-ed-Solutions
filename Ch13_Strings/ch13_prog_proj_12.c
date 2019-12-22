23b109ae88c6f3538886d297c68cce3c490a220a
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-22 10:34:18 +0200

    add ch13 prog proj 12

---
+++ Ch13_Strings/ch13_prog_proj_12.c
@@ -0,0 +1,61 @@
+/*
+ * ch13_prog_proj_12.c
+ *
+ *  Created on: Dec 22, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 12: Reverse words in a sentence
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define WORDS_NUM 30
+#define WORDS_LEN 20
+
+bool end_with_special_ch(char *str, char *special_ch);
+
+int main(void)
+{
+	char sentence[WORDS_NUM][WORDS_LEN + 1], special_ch;
+	int i;
+
+	printf("Enter a sentence: ");
+	fflush(stdout);
+	fflush(stdin);
+
+	for(i = 0; i < WORDS_NUM; i++)
+	{
+		scanf("%s", sentence[i]);
+        
+		if(end_with_special_ch(sentence[i], &special_ch))
+			break;
+	}
+
+	if(i == WORDS_NUM)
+		i--;
+
+	// Printing the reversed sentence
+	while(i > 0)
+	{
+		printf("%s ", sentence[i--]);
+	}
+	printf("%s%c\n", sentence[i], special_ch);
+
+	return 0;
+}
+
+bool end_with_special_ch(char *str, char *special_ch)
+{
+	while(*str++);
+	str -= 2; // Go back to the last character
+
+	if(*str == '.' || *str == '!' || *str == '?')
+	{
+		*special_ch = *str;
+		*str = '\0';
+		return true;
+	}
+
+	return false;
+}
