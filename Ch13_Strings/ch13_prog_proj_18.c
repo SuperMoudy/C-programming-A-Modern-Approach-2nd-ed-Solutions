8cc99dd6daf655574449e7478afc8111ea8c345f
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-23 04:55:21 +0200

    add ch13 prog proj 18

---
+++ Ch13_Strings/ch13_prog_proj_18.c
@@ -0,0 +1,30 @@
+/*
+ * ch13_prog_proj_18.c
+ *
+ *  Created on: Dec 22, 2019
+ *      Author: SuperMoudy
+ */
+
+// Programming Project 18: Date Format
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	int dd, mm, yyyy;
+	char *months[] =
+	{
+			"January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+	};
+
+	printf("Enter a data (mm/dd/yyyy): ");
+	fflush(stdout);
+	fflush(stdin);
+	scanf("%d / %d / %d", &mm, &dd, &yyyy);
+
+	printf("You entered the data %s %d, %d", months[mm - 1], dd, yyyy);
+
+	return 0;
+}
