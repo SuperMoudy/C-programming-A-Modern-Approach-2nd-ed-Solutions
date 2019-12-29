e372e7edf2f9c24b2af60ad355bbb23fae034ab7
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-29 20:00:48 +0200

    add ch15 prog proj 2

---
+++ Ch15_Writing_Large_Programs/ch15_prog_proj_02/word.h
@@ -0,0 +1,21 @@
+/*
+ * word.h
+ *
+ *  Created on: Dec 29, 2019
+ *      Author: SuperMoudy
+ */
+
+#ifndef WORD_H_
+#define WORD_H_
+
+#define MAX_WORD_LEN 20
+
+/*********************************************************************************
+ * read_word: Reads the next word from the input and stores it in word.          *
+ *            Makes word empty if no word could be read because of end-of-file.  *
+ * 			  Truncates the word if its length exceeds len.                      *
+ *********************************************************************************/
+void read_word(char *word, int len);
+
+
+#endif /* WORD_H_ */
