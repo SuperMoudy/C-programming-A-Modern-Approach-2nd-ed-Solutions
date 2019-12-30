3a17c47351d838eac8ba4c652e36ebd340dd8fe6
SuperMoody <mahmoudhamdy.eeng@gmail.com>
2019-12-30 09:12:07 +0200

    add ch15 prog proj 4

---
+++ Ch15_Writing_Large_Programs/ch15_prog_proj_04/readline.c
@@ -0,0 +1,21 @@
+/*
+ * readline.c
+ *
+ *  Created on: Dec 30, 2019
+ *      Author: SuperMoudy
+ */
+
+#include <stdio.h>
+#include "readline.h"
+
+int read_line(char str[], int n)
+{
+	int ch, i = 0;
+
+	while((ch = getchar()) != '\n')
+		if(i < n)
+			str[i++] = ch;
+	str[i] = '\0';
+
+	return i;
+}
---
+++ Ch15_Writing_Large_Programs/ch15_prog_proj_04/readline.h
@@ -0,0 +1,13 @@
+/*
+ * readline.h
+ *
+ *  Created on: Dec 30, 2019
+ *      Author: SuperMoudy
+ */
+
+#ifndef READLINE_H_
+#define READLINE_H_
+
+int read_line(char str[], int n);
+
+#endif /* READLINE_H_ */
