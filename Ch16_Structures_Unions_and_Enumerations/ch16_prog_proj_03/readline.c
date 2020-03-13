71dc55fab79707963d436a6699cb6eb6e485408d
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-03-13 10:03:36 +0200

    add ch16 prog proj 3

---
+++ Ch16_Structures_Unions_and_Enumerations/ch16_prog_proj_03/readline.c
@@ -0,0 +1,28 @@
+/*
+ * readline.c
+ *
+ *  Created on: Mar 13, 2020
+ *      Author: SuperMoudy
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include "readline.h"
+
+int read_line(char str[], int n)
+{
+	int ch, i = 0;
+
+	while(isspace(ch = getchar()));
+
+	while(ch != '\n' && ch != EOF)
+	{
+		if(i < n)
+			str[i++] = ch;
+
+		ch = getchar();
+	}
+	str[i] = '\0';
+
+	return i;
+}
