05035e028fec7369a0af51cfc8b9a63727e05fd4
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-04-07 10:44:45 +0200

    add ch17 prog proj 1

---
+++ Ch17_Advanced_Uses_of_Pointers/ch17_prog_proj_01/readline.c
@@ -0,0 +1,28 @@
+/*
+ * readline.c
+ *
+ *  Created on: Apr 7, 2020
+ *      Author: SuperMoudy
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include "readline.h"
+
+int read_line(char str[], int n)
+{
+	int ch, i = 0;
+
+	while(isspace(ch = getchar()));
+
+	while(ch != '\n' && ch != EOF)
+	{
+		if(i < n)
+			str[i++] = ch;
+
+		ch = getchar();
+	}
+	str[i] = '\0';
+
+	return i;
+}
