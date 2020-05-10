dee93dcb14560bce3b78b51523150dd79c375136
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-05-10 07:46:55 +0200

    add copyright for ch19 exercises

--- Ch19_Program_Design/ch19_exercise_06/a/stackADT.c
+++ Ch19_Program_Design/ch19_exercise_06/a/stackADT.c
@@ -1,3 +1,14 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+// (Section 19.4, Page 495)
+
 /*
  * stackADT.c
  *
