a584817f8f43cd7b670d4fcbcdaf036a7824ebbd
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2022-01-06 08:10:22 +0200

    change ch5 prog proj 8 solution

--- Ch05_Selection_Statements/ch5_prog_proj_08.c
+++ Ch05_Selection_Statements/ch5_prog_proj_08.c
@@ -8,69 +8,93 @@
 // Programming Project 8: Daily Flights
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	int desired_time, best_time_index = -1, time_range;
-	int hh_desired, mm_desired, hh_dep, mm_dep, hh_arrive, mm_arrive;
-	char dep_meridiem, arrive_meridiem;
+	int hh_desired, mm_desired, desired_time;
+	int hh_dep, mm_dep, closest_dep_time;
+	int hh_arrive, mm_arrive, arrive_time;
+	int min_time_diff = 1440; // Assuming the start value is 24 hr diff (max diff)
+	char dep_meridiem, arrive_meridiem; // a.m or p.m?
 
-	// Computing time in terms of total minutes starting from 00:00
-	int dep_time[8] =
-	{
-			480,  // 08:00 a.m.
-			583,  // 09:43 a.m.
-			679,  // 11:19 a.m.
-			767,  // 12:47 p.m.
-			840,  // 02:00 p.m.
-			945,  // 03:45 p.m.
-			1140, // 07:00 p.m.
-			1305  // 09:45 p.m.
-	};
-
-	int arrival_time[8] =
-	{
-			616,  // 10:16 a.m.
-			712,  // 11:52 a.m.
-			811,  // 01:31 p.m.
-			900,  // 03:00 p.m.
-			968,  // 04:08 p.m.
-			1075, // 05:55 p.m.
-			1280, // 09:20 p.m.
-			1438  // 11:58 p.m.
-	};
-
+	// Reading user input (desired time)
 	printf("Enter a 24-hour time: ");
 	scanf("%d:%d", &hh_desired, &mm_desired);
 
+	// Convert the desired time from hh:mm format to total minutes format
 	desired_time = hh_desired * 60 + mm_desired;
 
-	for(int i = 0; i < 7; i++)
+	// Finding the closest time to our desired time according to the given table
+	// 08:00 a.m.
+	if(min_time_diff > abs(480 - desired_time))
 	{
-		time_range = dep_time[i] + (dep_time[i + 1] - dep_time[i]) / 2;
-		if(desired_time > dep_time[i] && desired_time <= time_range)
-		{
-			best_time_index = i;
-			break;
-		}
+		min_time_diff = abs(480 - desired_time); // update new min diff
+		closest_dep_time = 480; // 08:00 a.m.
+		arrive_time = 616; // 10:16 a.m.
 	}
 
-	if(best_time_index == -1)
+	// 09:43 a.m.
+	if(min_time_diff > abs(583 - desired_time))
 	{
-		if(desired_time <= 172) // 2:52 am --> (9:45 pm < 2:52 am < 8:00 am)
-		{
-			best_time_index = 7;
-		}
-		else
-		{
-			best_time_index = 0;
-		}
+		min_time_diff = abs(583 - desired_time); // update new min diff
+		closest_dep_time = 583; //  09:43 a.m.
+		arrive_time = 712; // 11:52 a.m.
 	}
 
-	hh_dep = dep_time[best_time_index] / 60;
-	mm_dep = dep_time[best_time_index] % 60;
+	// 11:19 a.m.
+	if(min_time_diff > abs(679 - desired_time))
+	{
+		min_time_diff = abs(679 - desired_time); // update new min diff
+		closest_dep_time = 679; // 11:19 a.m.
+		arrive_time = 811; // 01:31 p.m.
+	}
 
+	// 12:47 p.m.
+	if(min_time_diff > abs(767 - desired_time))
+	{
+		min_time_diff = abs(767 - desired_time); // update new min diff
+		closest_dep_time = 767; // 12:47 p.m.
+		arrive_time = 900; // 03:00 p.m.
+	}
+
+	// 02:00 p.m.
+	if(min_time_diff > abs(840 - desired_time))
+	{
+		min_time_diff = abs(840 - desired_time); // update new min diff
+		closest_dep_time = 840; // 02:00 p.m.
+		arrive_time = 968; // 04:08 p.m.
+	}
+
+	// 03:45 p.m.
+	if(min_time_diff > abs(945 - desired_time))
+	{
+		min_time_diff = abs(945 - desired_time); // update new min diff
+		closest_dep_time = 945; // 03:45 p.m.
+		arrive_time = 1075; // 05:55 p.m.
+	}
+
+	// 07:00 p.m.
+	if(min_time_diff > abs(1140 - desired_time))
+	{
+		min_time_diff = abs(1140 - desired_time); // update new min diff
+		closest_dep_time = 1140; // 07:00 p.m.
+		arrive_time = 1280; // 09:20 p.m.
+	}
+
+	// 09:45 p.m.
+	if(min_time_diff > abs(1305 - desired_time))
+	{
+		min_time_diff = abs(1305 - desired_time); // update new min diff
+		closest_dep_time = 1305; // 09:45 p.m.
+		arrive_time = 1438; // 11:58 p.m.
+	}
+
+	// Convert back the departure time from minutes format to hh:mm format
+	hh_dep = closest_dep_time / 60;
+	mm_dep = closest_dep_time % 60;
+
+	// Convert from 24-hr format to 12-hr format with meridiem
 	if(hh_dep == 0) // Midnight
 	{
 		hh_dep += 12;
@@ -88,11 +112,11 @@ int main(void)
 		dep_meridiem = 'a';
 	}
 
+	// Convert back the arrival time from minutes format to hh:mm format
+	hh_arrive = arrive_time / 60;
+	mm_arrive = arrive_time % 60;
 
-
-	hh_arrive = arrival_time[best_time_index] / 60;
-	mm_arrive = arrival_time[best_time_index] % 60;
-
+	// Convert from 24-hr format to 12-hr format with meridiem
 	if(hh_arrive == 0) // Midnight
 	{
 		hh_arrive += 12;
@@ -116,3 +140,4 @@ int main(void)
 
 	return 0;
 }
+
