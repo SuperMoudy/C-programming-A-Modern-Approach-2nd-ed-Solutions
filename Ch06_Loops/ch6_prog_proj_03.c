56f821320f66437f24d6e7122e7137c7508214ec
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2024-03-12 20:46:27 +0200

    update ch6 prog proj 3

--- Ch06_Loops/ch6_prog_proj_03.c
+++ Ch06_Loops/ch6_prog_proj_03.c
@@ -6,7 +6,34 @@
  */
 
 // Programming Project 3: Fraction's lowest terms (GCD)
+// New solution from 2024 (related to programming project 2)
+#include <stdio.h>
 
+int main(void)
+{
+	int numerator, denominator, m, n, remainder;
+
+	printf("Enter a fracation: ");
+	scanf("%d / %d", &numerator, &denominator);
+
+	m = numerator;
+	n = denominator;
+
+	while(n != 0)
+	{
+		remainder = m % n;
+		m = n;
+		n = remainder;
+	}
+
+	printf("In lowest terms: %d/%d\n", numerator / m, denominator / m);
+
+	return 0;
+}
+
+
+// Old solution from 2019
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -49,3 +76,4 @@ int main(void)
 
 	return 0;
 }
+*/
