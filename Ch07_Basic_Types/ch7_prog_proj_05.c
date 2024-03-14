348c9d52ab1b0c8b313469332f36c817877ccb20
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2024-03-14 14:42:17 +0200

    update ch7 prog proj 5: adjust the main solution

--- Ch07_Basic_Types/ch7_prog_proj_05.c
+++ Ch07_Basic_Types/ch7_prog_proj_05.c
@@ -7,35 +7,6 @@
 
 // Programming Project 5: Scrabble
 
-// Faster implementation (Tradeoff between memory and speed)
-#include <stdio.h>
-#include <ctype.h>
-
-int main(void)
-{
-	int face_value[26] =
-	{
-			1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
-	};
-
-	char input_ch;
-	int score = 0;
-
-	printf("Enter a word: ");
-
-	while((input_ch = getchar()) != '\n')
-	{
-		if(input_ch >= 'A' && input_ch <= 'z')
-			score += face_value[toupper(input_ch) - 'A'];
-	}
-
-	printf("Scrabble value: %d\n", score);
-
-	return 0;
-}
-
-// Slower Implementation (Treadeoff between memory and speed)
-/*
 #include <stdio.h>
 #include <ctype.h>
 
@@ -66,6 +37,36 @@ int main(void)
 		case 'Q': case 'Z': score += 10; break;
 
 		}
+	}
+
+	printf("Scrabble value: %d\n", score);
+
+	return 0;
+}
+
+
+// Faster implementation using arrays (to be studied in ch8)
+// (Tradeoff between memory and speed)
+/*
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int face_value[26] =
+	{
+			1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+	};
+
+	char input_ch;
+	int score = 0;
+
+	printf("Enter a word: ");
+
+	while((input_ch = getchar()) != '\n')
+	{
+		if(input_ch >= 'A' && input_ch <= 'z')
+			score += face_value[toupper(input_ch) - 'A'];
 	}
 
 	printf("Scrabble value: %d\n", score);
